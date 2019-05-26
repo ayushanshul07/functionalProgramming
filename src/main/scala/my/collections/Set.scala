19a7e1a75c97f248ee4f877440f6a4b599ac4b6e
Ayush Anshul <ayushanshul07@gmail.com>
2019-05-26 13:48:25 +0530

    Functional Sets

--- src/main/scala/my/collections/Set.scala
+++ src/main/scala/my/collections/Set.scala
@@ -1,3 +1,28 @@
 package my.collections
 
-object Set
+trait Set extends (String => Boolean) {
+
+  final def add(input: String): Set = element =>
+    (element == input) || this(element)
+
+  final def remove(input: String): Set = element =>
+    (element != input) && this(element)
+
+  final def union(that: Set): Set = element =>
+    this(element) || that(element)
+
+  final def intersection(that: Set): Set = element =>
+    this(element) && that(element)
+
+  final def difference(that: Set): Set = element =>
+    this(element) && !that(element)
+
+  final def isSubsetOf(that: Set): Set = element => ??? //todo
+
+}
+
+object Set {
+
+  val empty: Set = _ => false
+
+}
--- src/test/scala/my/collections/SetTests.scala
+++
@@ -1,1 +0,0 @@
-import my.collections._
